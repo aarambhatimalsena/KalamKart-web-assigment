bd48ace0680a130c40c22ea5fce1c382021412fc
Aarambha Timalsena <timalsenaaarambha75@gmail.com>
2025-06-25 07:35:20 +0545

    Created main App component and route structure

---
+++ frontend/src/router/AdminRoute.jsx
@@ -0,0 +1,32 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const AdminRoute = () => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  const [redirectPath, setRedirectPath] = useState(null);
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        toast.error("Please login first");
+        setRedirectPath("/login");
+      } else if (!(user?.isAdmin || user?.role === "admin")) {
+        toast.error("Access denied! Admins only.");
+        setRedirectPath("/dashboard");
+      }
+    }
+  }, [user, loading]);
+
+  if (loading) return <div className="text-center py-10">Loading...</div>;
+
+  if (redirectPath) {
+    return <Navigate to={redirectPath} state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AdminRoute;
